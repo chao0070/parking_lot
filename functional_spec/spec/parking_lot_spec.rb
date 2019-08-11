a1d1c3e18f3138117013c430346fee7d02d2d978
Mohit Agarwal <mohitgarg0070@gmail.com>
2019-08-12 05:08:25 +0530

    Updated functional spec

--- functional_spec/spec/parking_lot_spec.rb
+++ functional_spec/spec/parking_lot_spec.rb
@@ -36,5 +36,61 @@ EOTXT
 )
   end
 
+  it "when full tells no more space" do
+    run_command(pty, "park KA-01-HH-1234 White\n")
+    run_command(pty, "park KA-01-HH-3141 Black\n")
+    run_command(pty, "park KA-01-HH-9999 White\n")
+    run_command(pty, "park KA-01-HH-3131 Black\n")
+    expect(fetch_stdout(pty)).to end_with("Sorry, parking lot is full\n")
+  end
+
+  it "allows search for reg no based on car color" do
+    run_command(pty, "park KA-01-HH-1234 White\n")
+    run_command(pty, "park KA-01-HH-3141 Black\n")
+    run_command(pty, "park KA-01-HH-9999 White\n")
+    run_command(pty, "registration_numbers_for_cars_with_colour White\n")
+    expect(fetch_stdout(pty)).to end_with("KA-01-HH-1234, KA-01-HH-9999\n")
+  end
+
+  it "allows search for slot no based on car color" do
+    run_command(pty, "park KA-01-HH-1234 White\n")
+    run_command(pty, "park KA-01-HH-3141 Black\n")
+    run_command(pty, "park KA-01-HH-9999 White\n")
+    run_command(pty, "slot_numbers_for_cars_with_colour White\n")
+    expect(fetch_stdout(pty)).to end_with("1, 3\n")
+  end
+
+  it "allows search of slot number based on reg number" do
+    run_command(pty, "park KA-01-HH-1234 White\n")
+    run_command(pty, "park KA-01-HH-3141 Black\n")
+    run_command(pty, "park KA-01-HH-9999 White\n")
+    run_command(pty, "slot_number_for_registration_number KA-01-HH-1234\n")
+    expect(fetch_stdout(pty)).to end_with("1\n")
+  end
+
+  it "gives errors when it doesn't find things in search" do
+    run_command(pty, "park KA-01-HH-1234 White\n")
+    run_command(pty, "park KA-01-HH-3141 Black\n")
+    run_command(pty, "park KA-01-HH-9999 White\n")
+    run_command(pty, "slot_numbers_for_cars_with_colour Green\n")
+    expect(fetch_stdout(pty)).to end_with("Not found\n")
+  end
+
+  it "gives errors when it doesn't find things in search" do
+    run_command(pty, "park KA-01-HH-1234 White\n")
+    run_command(pty, "park KA-01-HH-3141 Black\n")
+    run_command(pty, "park KA-01-HH-9999 White\n")
+    run_command(pty, "registration_numbers_for_cars_with_colour Green\n")
+    expect(fetch_stdout(pty)).to end_with("Not found\n")
+  end
+
+  it "gives errors when it doesn't find things in search" do
+    run_command(pty, "park KA-01-HH-1234 White\n")
+    run_command(pty, "park KA-01-HH-3141 Black\n")
+    run_command(pty, "park KA-01-HH-9999 White\n")
+    run_command(pty, "slot_number_for_registration_number KA-01-HH-12345\n")
+    expect(fetch_stdout(pty)).to end_with("Not found\n")
+  end
+
   # pending "add more specs as needed"
 end
